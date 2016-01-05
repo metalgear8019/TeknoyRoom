7176fbcd3e1ebc3b58f6b96b02c8d55246ec9ec9
metalgear8019 <metalgear8019@github.com>
2016-01-05 15:59:14 +0800

    Initial UI integration

--- client/app.js
+++ client/app.js
@@ -3,6 +3,12 @@ Session.setDefault('counter', 0);
 Template.body.events
 (
 	{
+		'click #menu-toggle': function (event)
+		{
+			event.preventDefault();
+			$("#wrapper").toggleClass("toggled");
+			alert('toggle class');
+		},
 		'submit #frmUser': function (event)
 		{
 			event.preventDefault();
