dbfbbf51e7c4ce54a028f1ec314485d899291804
metalgear8019 <metalgear8019@github.com>
2016-01-05 18:49:51 +0800

    Integrated add course function

--- client/app.js
+++ client/app.js
@@ -73,34 +73,6 @@ Template.body.events
 			}
 		},
 
-		'submit #frmCourse': function (event)
-		{
-			event.preventDefault();
-
-			var subject_number = event.target.subject_number.value;
-			var title = event.target.title.value;
-			var unit = event.target.unit.value;
-
-			if (subject_number != '' || title != '' || unit != '') 
-			{
-				var course = {
-								subject_number: subject_number,
-								title: title,
-								unit: unit
-							 };
-
-				Meteor.call('addCourse', course);
-
-				event.target.subject_number.value = '';
-				event.target.title.value = '';
-				event.target.unit.value = '';
-			}
-			else
-			{
-				alert('Necessary fields must be filled..');
-			}
-		},
-
 		'submit #frmSemester': function (event)
 		{
 			event.preventDefault();
