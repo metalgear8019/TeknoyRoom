4de7845be630db42204c9ad8be181f5c015ecafc
Ann <annalim.me@gmail.com>
2016-01-19 20:11:21 +0800

    added tooltip and disabled button attributes

---
+++ client/components/instructor/instructor-currentcourses-enterclass.js
@@ -0,0 +1,23 @@
+
+Template.instructorEnterClass.events
+(
+	{
+		'click #share-screen': function (event)
+		{
+			event.preventDefault();
+			$('#share-camera').removeAttr('disabled');
+			$('#share-screen').attr('disabled', 'disabled');
+		},
+		'click #share-camera': function (event)
+		{
+			event.preventDefault();
+			$('#share-screen').removeAttr('disabled');
+			$('#share-camera').attr('disabled', 'disabled');
+		}
+	}
+);
+
+/*$("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+});*/
